ecc54b2806b5bc8c6719684b12a13c26adb35baf
Hugo Almeida <hugomurilo.almeida@gmail.com>
2023-09-18 11:28:47 -0300

    colocando a anotação @RequestMapping no UsuarioController para evitar repetição de código

--- SpringBoot/my-first-web-api/src/main/java/dio/web/api/controller/UsuarioController.java
+++ SpringBoot/my-first-web-api/src/main/java/dio/web/api/controller/UsuarioController.java
@@ -8,26 +8,29 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UsuarioController {
 
     // o repository tem a lógica de negócio, por isso a injeção
     @Autowired
     private UsuarioRepository repository;
-    @GetMapping("/users")
+    @GetMapping()
     public List<Usuario> getUsers(){
         return repository.findAll();
     }
-    @GetMapping("/users/{username}")
+    @GetMapping("/{username}")
     public Usuario getOne(@PathVariable("username") String username){
         //o nome que vier na URI será passada como argumento no método
         return repository.findByUsername(username);
     }
-    @DeleteMapping("/users/{id}")
+    @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable("id") Integer id){
         repository.deleteById(id);
     }
-    @PostMapping("/users")
+    @PostMapping()
     public void postUser(@RequestBody Usuario usuario){
         repository.save(usuario);
     }
+
+    // como todos terão um padrão iniciando "/users", pode-se usar a anotação @RequestMapping
 }
